73f226360227d3966bdcf4523f25fc2a2eb0fa0b
andrewkorepanov <andrew.korepanov@imotions.com>
2023-05-31 11:36:16 +0200

    no pangolin

---
+++ cmake/_FindPangolin.cmake
@@ -0,0 +1,18 @@
+set(Pangolin_INCLUDE_DIRS 
+  /home/me/work/Pangolin/components/pango_core/include;
+  /home/me/work/Pangolin/components/pango_display/include;
+  /home/me/work/Pangolin/components/pango_geometry/include;
+  /home/me/work/Pangolin/components/pango_glgeometry/include;
+  /home/me/work/Pangolin/components/pango_image/include;
+  /home/me/work/Pangolin/components/pango_opengl/include;
+  /home/me/work/Pangolin/components/pango_packetstream/include;
+  /home/me/work/Pangolin/components/pango_plot/include;
+  /home/me/work/Pangolin/components/pango_python/include;
+  /home/me/work/Pangolin/components/pango_scene/include;
+  /home/me/work/Pangolin/components/pango_tools/include;
+  /home/me/work/Pangolin/components/pango_vars/include;
+  /home/me/work/Pangolin/components/pango_video/include;
+  /home/me/work/Pangolin/components/pango_windowing/include;
+  /home/me/work/Pangolin/components/tinyobj/include
+)
+set(Pangolin_FOUND TRUE)
